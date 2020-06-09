81ccdaf8fd06a3235381c7a0e7fb40d2b369c12a
Андрей <zybarevandrey@gmail.com>
2020-06-09 10:07:36 +0300

    Main screen. Scroll View Model.

---
+++ OneRM/OneRM/ViewModels/ExerciseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace OneRM.ViewModels
+{
+    public class ExerciseViewModel : ObservableObject
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Weight { get; set; }
+        public string HeroColor { get; set; }
+        public bool IsFeatured { get; set; }
+    }
+}
---
+++ OneRM/OneRM/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmHelpers;
+
+namespace OneRM.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public IList<ExerciseViewModel> Exercises { get; set; }
+        public MainViewModel()
+        {
+            Exercises = new ObservableRangeCollection<ExerciseViewModel>()
+            {
+                new ExerciseViewModel()
+                {
+                    Name = "Присед",
+                    HeroColor = "#96C9F8",
+                    ImageUrl = "",
+                    Weight = 0,
+                    IsFeatured = true
+                },
+                new ExerciseViewModel()
+                {
+                    Name = "Становая",
+                    HeroColor = "#FFCA81",
+                    ImageUrl = "",
+                    Weight = 0,
+                    IsFeatured = true
+                },
+                new ExerciseViewModel()
+                {
+                    Name = "Румынская",
+                    HeroColor = "#D69DFB",
+                    ImageUrl = "",
+                    Weight = 0,
+                    IsFeatured = true
+                },
+                 new ExerciseViewModel()
+                {
+                    Name = "Жим",
+                    HeroColor = "#74D59E",
+                    ImageUrl = "",
+                    Weight = 0,
+                    IsFeatured = true
+                }
+            };
+        }
+    }
+}
