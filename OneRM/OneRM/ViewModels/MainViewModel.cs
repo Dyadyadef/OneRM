88d3ed37f19545eb758dced375232944f64c8c85
Андрей <zybarevandrey@gmail.com>
2020-06-17 09:48:24 +0300

    Синхронизация с домом

--- OneRM/OneRM/ViewModels/MainViewModel.cs
+++ OneRM/OneRM/ViewModels/MainViewModel.cs
@@ -36,7 +36,39 @@ namespace OneRM.ViewModels
                     Weight = 0,
                     IsFeatured = false
                 },
-                 new ExerciseViewModel()
+                new ExerciseViewModel()
+                {
+                    Name = "Жим",
+                    HeroColor = "#74D59E",
+                    ImageUrl = "green_moss",
+                    Weight = 0,
+                    IsFeatured = true
+                },
+                new ExerciseViewModel()
+                {
+                    Name = "Присед",
+                    HeroColor = "#96C9F8",
+                    ImageUrl = "blue_moss",
+                    Weight = 0,
+                    IsFeatured = true
+                },
+                new ExerciseViewModel()
+                {
+                    Name = "Становая",
+                    HeroColor = "#FFCA81",
+                    ImageUrl = "orange_moss",
+                    Weight = 0,
+                    IsFeatured = false
+                },
+                new ExerciseViewModel()
+                {
+                    Name = "Румынская",
+                    HeroColor = "#D69DFB",
+                    ImageUrl = "lavender_moss",
+                    Weight = 0,
+                    IsFeatured = false
+                },
+                new ExerciseViewModel()
                 {
                     Name = "Жим",
                     HeroColor = "#74D59E",
